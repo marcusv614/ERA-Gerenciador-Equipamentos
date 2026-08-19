3aaaf1c1e4fd4a310a9e2571c1515ba25bbadc62
Vini ERA PC <marcus.pinheiro@super.ufam.edu.br>
2026-08-19 16:17:50 -0300

    feat(database): add employees and works

---
+++ src/main/resources/db/migration/V9__adicionar_funcionarios_e_obras.sql
@@ -0,0 +1,19 @@
+INSERT INTO funcionarios (nome, cargo, email, telefone, status)
+VALUES
+    ('Jander', 'Técnico de campo', 'jander@era.com.br', NULL, 'Ativo'),
+    ('Rodrigo Paes', 'Técnico de campo', 'rodrigo.paes@era.com.br', NULL, 'Ativo'),
+    ('Eduardo Araujo', 'Técnico de campo', 'eduardo.araujo@era.com.br', NULL, 'Ativo'),
+    ('Adriel Cruz', 'Técnico de campo', 'adriel.cruz@era.com.br', NULL, 'Ativo'),
+    ('Denis', 'Técnico de campo', 'denis@era.com.br', NULL, 'Ativo'),
+    ('Islando', 'Técnico de campo', 'islando@era.com.br', NULL, 'Ativo'),
+    ('Marcos', 'Técnico de campo', 'marcos@era.com.br', NULL, 'Ativo'),
+    ('Jonas', 'Técnico de campo', 'jonas@era.com.br', NULL, 'Ativo'),
+    ('Thaisa', 'Técnico de campo', 'thaisa@era.com.br', NULL, 'Ativo');
+
+INSERT INTO obras (nome, cliente, cidade, inicio, status)
+VALUES
+    ('GlobalStar', 'GlobalStar', 'Manaus', CURRENT_DATE, 'Planejada'),
+    ('P&G Rio Negro', 'P&G', 'Manaus', CURRENT_DATE, 'Planejada'),
+    ('P&G Solimões', 'P&G', 'Manaus', CURRENT_DATE, 'Planejada'),
+    ('Fametro', 'Fametro', 'Manaus', CURRENT_DATE, 'Planejada'),
+    ('Antonelly', 'Antonelly', 'Manaus', CURRENT_DATE, 'Planejada');
