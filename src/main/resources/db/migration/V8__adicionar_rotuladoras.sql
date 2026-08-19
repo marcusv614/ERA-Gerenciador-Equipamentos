6262bc130baa17881231edf98cdf5301eed908ff
Vini ERA PC <marcus.pinheiro@super.ufam.edu.br>
2026-08-19 16:15:32 -0300

    feat(database): add label printers

---
+++ src/main/resources/db/migration/V8__adicionar_rotuladoras.sql
@@ -0,0 +1,7 @@
+INSERT INTO equipamentos
+    (tipo, modelo, serie, status, data_entrada, quantidade, quantidade_reservada, controle_quantidade)
+VALUES
+    ('ROTULADORA', 'Rotuladora', '303-303', 'Em estoque', CURRENT_DATE, 1, 0, 'INDIVIDUAL'),
+    ('ROTULADORA', 'Rotuladora', '404-404', 'Em estoque', CURRENT_DATE, 1, 0, 'INDIVIDUAL'),
+    ('ROTULADORA', 'Rotuladora', '505-505', 'Em estoque', CURRENT_DATE, 1, 0, 'INDIVIDUAL'),
+    ('ROTULADORA', 'Rotuladora', '606-606', 'Em estoque', CURRENT_DATE, 1, 0, 'INDIVIDUAL');
